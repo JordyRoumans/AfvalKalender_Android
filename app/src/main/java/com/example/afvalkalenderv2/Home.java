765103d10fbedd612ffc76f30924e34ae5e15880
jordy <jordy.roumans@hotmail.com>
2022-01-24 14:17:39 +0100

    Added search functionality and notification timing settings

---
+++ app/src/main/java/com/example/afvalkalenderv2/Home.java
@@ -0,0 +1,59 @@
+package com.example.afvalkalenderv2;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.preference.PreferenceManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class Home extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        //navigation to settings fragment
+        NavController navController = Navigation.findNavController(view);
+
+        FloatingActionButton button = view.findViewById(R.id.floatingActionButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.NavigateToSettings);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/afvalkalenderv2/MainActivity.java
+++ app/src/main/java/com/example/afvalkalenderv2/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.preference.PreferenceManager;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,37 +32,40 @@ import java.io.InputStreamReader;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Year;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+
     List summaryToday,summaryTomorrow;
     int hours,minutes;
-    //get current date
+    //get datetimeformatter
     DateTimeFormatter dtf = DateTimeFormatter.BASIC_ISO_DATE;
-    //format the date
-    String datum = dtf.format(LocalDate.now());
     //get tomorrow date
     Date dt = new Date();
     //format the date
     String tomorrow = dtf.format(LocalDateTime.from(dt.toInstant().atZone(ZoneId.of("UTC"))).plusDays(1));
     String dayAfterTomorrow = dtf.format(LocalDateTime.from(dt.toInstant().atZone(ZoneId.of("UTC"))).plusDays(2));
+    //get a calender for the timer
+    Calendar calender = Calendar.getInstance();
+    //get datepicker
+    DatePickerDialog.OnDateSetListener setListener;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         //check build version
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
@@ -66,51 +75,51 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
-        //bind button and textview
-        btn = (Button) findViewById(R.id.button_1);
 
 
 
-
-
-        //event listener
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-
-
-
-
-
-
-
-
-                }
-
-
-        });
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void getTodayAndTomorrow()
+    {
         TextView txtSummaryToday = (TextView) findViewById(R.id.textViewSummaryToday);
         TextView txtSummaryTomorrow = (TextView) findViewById(R.id.textViewSummaryTomorrow);
-
         DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
         summaryToday = databaseHelper.SelectFromDate(tomorrow);
         summaryTomorrow = databaseHelper.SelectFromDate(dayAfterTomorrow);
         databaseHelper.close();
         txtSummaryToday.setText(summaryToday.toString().replaceAll("\\[", "").replaceAll("\\]",""));
         txtSummaryTomorrow.setText(summaryTomorrow.toString().replaceAll("\\[", "").replaceAll("\\]",""));
+    }
 
-        Calendar calender = Calendar.getInstance();
+    private void loadSettings()
+    {
+        SharedPreferences manager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        hours = Integer.parseInt(manager.getString("Hour","12"));
+        minutes = Integer.parseInt(manager.getString("Minute","1"));
+    }
+
+    private void setCalender(Calendar c)
+    {
+        c.set(Calendar.HOUR_OF_DAY,hours);
+        c.set(Calendar.MINUTE,minutes);
+        //for higher accuracy
+        c.set(Calendar.SECOND,1);
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        loadSettings();
+        getTodayAndTomorrow();
+
         //set time for repeating notification
-        calender.set(Calendar.HOUR_OF_DAY,21);
-        calender.set(Calendar.MINUTE,45);
-        calender.set(Calendar.SECOND,1);
+        setCalender(calender);
 
         Intent intent = new Intent(getApplicationContext(),Notification_receiver.class);
         intent.setAction("MY_NOTIFICATION_MESSAGE");
@@ -121,12 +130,54 @@ public class MainActivity extends AppCompatActivity {
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calender.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
+
+
+        TextView txtSummarySearch = (TextView) findViewById(R.id.textViewSummarySearch);
+        EditText txtSearch = (EditText) findViewById(R.id.editTextTextPersonName5);
+        EditText datePick = findViewById(R.id.editTextDate);
+        Calendar datePicker = Calendar.getInstance();
+        int year = datePicker.get(Calendar.YEAR);
+        int month = datePicker.get(Calendar.MONTH);
+        int day = datePicker.get(Calendar.DAY_OF_MONTH);
+
+        datePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadSettings();
+                setCalender(calender);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Dialog_MinWidth,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = year +"-"+month+1+"-"+day;
+                List SummarySearch=null;
+
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+                txtSearch.setText(date);
+                SummarySearch = databaseHelper.SelectFromDate(date.toString());
+                txtSummarySearch.setText(SummarySearch.toString().replaceAll("\\[", "").replaceAll("\\]",""));
+
+                databaseHelper.close();
+
+            }
+        };
     }
 
-    public void Main()
-    {
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadSettings();
+        setCalender(calender);
+        getTodayAndTomorrow();
     }
+
 
     private String ParseDates(String dateToCheck) throws IOException {
         String summary=null;
